0af734c96123e9747edbc08f7b0b68cd53a305ca
Sydney Hopson <sydneyhopsonreignmobile@Sydneys-iMac.local>
2023-01-09 19:25:31 -0500

    finished

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,19 @@
-// Write your Character component here
+import React from "react";
+
+
+
+
+
+const Character = ({character}) => { 
+    
+    // console.log(character)
+    
+    
+    return (
+        <div className="Swc">
+            {/* <h1 className="Header">Characters</h1> */}
+            <h3>{character.name}</h3>
+        </div>
+    )
+}
+export default Character
